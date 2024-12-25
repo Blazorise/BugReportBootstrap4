297561a2e13d07319130f5b41e483c737bdbcdff
GitHub Action <action@github.com>
2024-12-25 00:37:32 +0000

    Publish template output for Bootstrap4

---
+++ BlazoriseBugReportBootstrap4_241225.Client/Components/TodoApp/Filter.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazoriseBugReportBootstrap4_241225.Client.Components.TodoApp;
+
+public enum Filter
+{
+    All,
+    Active,
+    Completed,
+}
---
+++ BlazoriseBugReportBootstrap4_241225.Client/Components/TodoApp/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazoriseBugReportBootstrap4_241225.Client.Components.TodoApp;
+
+public class Todo
+{
+    public bool Completed { get; set; }
+
+    public string? Description { get; set; }
+}
